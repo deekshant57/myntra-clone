e2a22a74d887f55af5c93ea9029f08e69d78a417
adnankhateeb <adnankhateeb21@gmail.com>
2022-05-04 03:55:20 +0530

    firebase integration for signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Products from "./components/Products";
+import Signup from "./components/Signup";
 import men from "./data/mensData.json";
 
 function App() {
    return (
       <div className="App">
-         <h1>Welcome to home page</h1>
-         <Products data={men}/>
+         {/* <h1>Welcome to home page</h1> */}
+         {/* <Products data={men}/> */}
+         <Routes>
+            <Route path="/men" element={<Products data={men} />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+         </Routes>
          {/* {Links} */}
          {/* {Routes} */}
 
